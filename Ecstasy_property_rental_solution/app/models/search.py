e25a1dede3ca7781b65f6a15c5da8dfcca7ca50b
Ibukun16 <ib.agunbiade18@gmail.com>
2025-01-17 08:31:18 +0100

    update the models function with the search module

---
+++ Ecstasy_property_rental_solution/app/models/search.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module that handles text search on the app
+"""
+from flask import current_app
+
+
+def add_to_index(index, model):
+    """Function that add words to the search storage db"""
+    if not current_app.elasticsearch:
+        return
+    payload = {}
+    for field in model.__searchable__:
+        payload[field] = getattr(model, field)
+    current_app.elasticsearch.index(index=index, id=model.id, document=payload)
+
+
+def remove_from_index(index, model):
+    """Function that remove words from the search storage db"""
+    if not current_app.elasticsearch:
+        return
+    current_app.elasticsearch.delete(index=index, id=model.id)
+
+
+def query_index(index, query, page, per_page):
+    """Function that retrieves words from the search storage db"""
+    if not current_app.elasticsearch:
+        return [], 0
+    search = current_app.elasticsearch.search(
+        index=index,
+        query={'multi_match': {'query': query, 'fields': ['*']}},
+        from_=(page - 1) * per_page,
+        size=per_page)
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    return ids, search['hits']['total']['value']
