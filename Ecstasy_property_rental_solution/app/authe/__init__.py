ac9db553e66121e00c83b1b0a927c0858086c7f2
Ibukun16 <ib.agunbiade18@gmail.com>
2025-01-17 08:37:55 +0100

    Update the authetication directory of the app

---
+++ Ecstasy_property_rental_solution/app/authe/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that initializes the authentication package
+"""
+from flask import Blueprint
+
+app_views = Blueprint('auth', __name__)
+
+from app.authe import access
